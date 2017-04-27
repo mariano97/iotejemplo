13886dece77f9df8b223473c60251ab69b9820e3
Mario Andres Serrano Urueña <mariano970419@gmail.com>
2017-04-27 08:10:39 -0500

    Add files via upload

---
+++ sketch_apr23a.ino
@@ -0,0 +1,26 @@
+double dato1, dato2, dato3, dato4;
+
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  dato1 = random(0,1024);
+  dato2 = random(0,1024);
+  dato3 = random(0,1024);
+  dato4 = random(0,1024);
+
+  String sl = String((dato1/1024.0)*8.0);
+  String sw = String((dato2/1024.0)*5.0);
+  String pl = String((dato3/1024.0)*7.0);
+  String pw = String((dato4/1024.0)*3.0);
+
+  Serial.println("{\"sl\":" + sl + ", \"sw\":" + sw +", \"pl\":" + pl + ", \"pw\":" + pw + "}");
+
+  delay(3000);  
+
+}
